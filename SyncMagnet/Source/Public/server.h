4457341e3dd17f31fe8550f93e50d8de6833e871
Helmsys <84701901+Helmssyss@users.noreply.github.com>
2023-09-22 00:58:38 +0300

    Update server.h

--- SyncMagnet/Source/Public/server.h
+++ SyncMagnet/Source/Public/server.h
@@ -4,15 +4,16 @@
 #include <fstream>
 #include <vector>
 
-#define OK_SEND_TO      "OK_SEND_TO"
-#define IMG_SEND        "IMG_SEND"
-#define OK_SEND         "OK_SEND"
-#define ACCEPT          "ACCEPT"
-#define DECLINE         "DECLINE"
-#define DEVICE_NAME     "DEVICE_NAME"
-#define FILE_BYTE       "FILE_BYTE"
-#define FILE_BYTE_TO    "FILE_BYTE_TO"
-#define FILE_CHUNK_SIZE 1024
+#define OK_SEND_TO       "OK_SEND_TO"
+#define IMG_SEND         "IMG_SEND"
+#define OK_SEND          "OK_SEND"
+#define ACCEPT           "ACCEPT"
+#define DECLINE          "DECLINE"
+#define DEVICE_NAME      "DEVICE_NAME"
+#define FILE_BYTE        "FILE_BYTE"
+#define FILE_BYTE_TO     "FILE_BYTE_TO"
+#define DISCONNECT       "DISCONNECT"
+#define FILE_CHUNK_SIZE  3072
 
 using namespace std;
 
@@ -22,22 +23,19 @@ class Server {
 	public:
 		Server();
 
-		void Setup(const char*& ip, const char*& port);
+		void Setup();
 		void Start();
 		void Stop();
 
 	private:
-		bool SaveImageData(const int& bufferSize, bool isImagePathToDesktop = false);
 		void CreateSaveImagePathFolder(const wchar_t* pathFolder);
 		void HandleFileTransfer(char* buffer, int& bufferSize);
-		string GetClientDeviceName(char* buffer, int& buffSize);
 		void SendClientFile(string& inputFolder, char* buffer, const int& bufferSize);
+		void SaveImageData(const int& bufferSize, const char* fileName, bool isImagePathToDesktop = false);
 		vector<string> MessageParse(string message, int& msgLen = _LENGTH, const char seperator = '|');
-	
-		const char* ip;
-		const char* port;
-		bool isRun;
-		bool isLoad = true;
+		string GetClientDeviceName(char* buffer, int& buffSize);
+		string GetIPv4();
+
 		string PcUserName;
 		SOCKET ServerSocket{};
 		SOCKET ClientSocket{};
