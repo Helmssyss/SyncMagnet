337af8d10cfd2a353eb82998b9ce1e54df6c0e05
Helmsys <84701901+Helmssyss@users.noreply.github.com>
2023-09-22 00:58:56 +0300

    Update console.h

--- SyncMagnet/Source/Public/console.h
+++ SyncMagnet/Source/Public/console.h
@@ -3,6 +3,8 @@
 #include <iostream>
 #include <string>
 
+using namespace std;
+
 namespace console {
     constexpr const char* PURPLE = "\033[95m";
     constexpr const char* CYAN = "\033[96m";
@@ -15,18 +17,26 @@ namespace console {
     constexpr const char* DEFAULT = "\033[0m";
     constexpr const char* BANNER = "-*-SyncMagnet-*-\n";
 
-    void Header() {
+    void banner() {
         printf("%s%s\n%s", PURPLE, BANNER, DEFAULT);
+    }
+
+    void Header() {
+        banner();
         printf("%s[0] %sGet File \t\t%s[1] %sSent File%s\n", RED, CYAN, RED, CYAN, DEFAULT);
         printf("%s[2] %sGet Screen Share%s\n", RED, CYAN, DEFAULT);
         printf("%s[x] %sClose%s\n", RED, CYAN, DEFAULT);
         printf("%s[?] %sTo be continued%s\n", RED, PURPLE, DEFAULT);
     }
 
-    void Body(char chr[]) {
+    void Body(string chr) {
         printf("\n%s[~] %sConnection Started\n", RED, PURPLE);
-        printf("%s[~] %sConnect From Your Address %s[%s]%s\n", RED, PURPLE, YELLOW, chr, DEFAULT);
+        printf("%s[~] %sConnect From Your Address >>%s%s%s<<%s\n", RED, PURPLE, YELLOW,chr.c_str(), PURPLE, DEFAULT);
         printf("%s[~] %sWaiting For Connection%s\n", RED, PURPLE, DEFAULT);
+    }
+
+    void ConnectedClientDisplay(const char* client) {
+        printf("\r%s[*] %sConnected Device >>%s%s%s<<%s\n", RED, PURPLE, YELLOW, client, PURPLE, DEFAULT);
     }
 
     std::string Input() {
@@ -43,9 +53,6 @@ namespace console {
         );
     }
 
-    void SaveFileQuestionDisplay() {
-        printf("%s[~] %sSave File [%sy%s/%sn%s]%s\n", RED, PURPLE, YELLOW, PURPLE, YELLOW, PURPLE, DEFAULT);
-    }
     void UploadFileDisplay(const int &uploadByte,const long &fileSize) {
         printf("\r%s[~] %s%.3f/%.3f %s%s Uploading%s",
             RED, YELLOW, (float)uploadByte / (1024 * 1024),
@@ -61,5 +68,21 @@ namespace console {
         );
     }
 
-    inline void AlertMessage(const char* msg) {printf("%s[~] %s%s%s\n", RED, PURPLE, msg, DEFAULT);}
+    void DownloadFileDisplay(const int& downloadByte, const long& fileSize) {
+        printf("\r%s[~] %s%.2f/%.2f %s%s Downloading File%s",RED,YELLOW,
+            (float)downloadByte / (1024 * 1024), (float)fileSize / (1024 * 1024),
+            PURPLE, (fileSize / (1024 * 1024)) >= 1 ? "MB" : "KB",DEFAULT);
+    }
+
+    inline void SaveFileQuestionDisplay() { printf("%s[~] %sSave File [%sy%s/%sn%s]%s\n", RED, PURPLE, YELLOW, PURPLE, YELLOW, PURPLE, DEFAULT); }
+    inline void AlertMessage(const char* msg) { printf("%s[~] %s%s%s\n", RED, PURPLE, msg, DEFAULT); }
+
+    void QuitMessage() {
+        system("cls");
+        banner();
+        printf("%s[!] %sDisconnected%s\n",RED,PURPLE,DEFAULT);
+        printf("%s[!] %sReopen to connect%s\n",RED,PURPLE,DEFAULT);
+        printf("%s[!] %sAny key and 'Enter'%s\n", RED, PURPLE, DEFAULT);
+    }
+
 }
