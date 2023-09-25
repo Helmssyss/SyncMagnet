9c8ea3a2561622b2c7bdd0bd54f3a3a45ed1f417
Helmsys <84701901+Helmssyss@users.noreply.github.com>
2023-09-25 11:15:04 +0300

    Update client_screen.h

--- Public/client_screen.h
+++ Public/client_screen.h
@@ -10,39 +10,6 @@ namespace Screen {
     const wchar_t winClass[] = L"ClientScreenClass";
     constexpr const wchar_t screenTitle[] = L"Empty Screen";
 
-    LRESULT CALLBACK ScreenWinProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
-        switch (uMsg) {
-            case WM_DESTROY:
-                DestroyWindow(hwnd);
-                PostQuitMessage(0);
-                break;
-
-            case WM_PAINT:
-                PAINTSTRUCT ps;
-                HDC hdc = BeginPaint(hwnd, &ps);
-                FillRect(hdc, &ps.rcPaint, (HBRUSH)(COLOR_WINDOW + 2));
-                EndPaint(hwnd, &ps);
-                break;
-        }
-        return DefWindowProc(hwnd, uMsg, wParam, lParam);
-    }
-
-    int WINAPI CreateScreen() {
-        WNDCLASS wc = {};
-        wc.lpfnWndProc = Screen::ScreenWinProc;
-        wc.hInstance = GetModuleHandle(NULL);
-        wc.lpszClassName = winClass;
-        RegisterClass(&wc);
-
-        HWND hwnd = CreateWindowEx(0, winClass, screenTitle, WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, 300, 700, NULL, NULL, GetModuleHandle(NULL), NULL);
-
-        ShowWindow(hwnd, SW_SHOW);
-        MSG msg{};
-        while (GetMessage(&msg, NULL, 0, 0) > 0) {
-            TranslateMessage(&msg);
-            DispatchMessage(&msg);
-        }
-
-        return 0;
-    }
+    LRESULT CALLBACK ScreenWinProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+    int WINAPI CreateScreen();
 }
