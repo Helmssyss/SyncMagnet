fd7c0d9ddef9fa1a981f6593c0bdcc7a27c9708e
Helmsys <84701901+Helmssyss@users.noreply.github.com>
2023-09-25 23:05:10 +0300

    Update server.h

--- Public/server.h
+++ Public/server.h
@@ -5,15 +5,15 @@
 #include <vector>
 
 #define OK_SEND_TO       "OK_SEND_TO"
-#define IMG_SEND         "IMG_SEND"
 #define OK_SEND          "OK_SEND"
 #define ACCEPT           "ACCEPT"
 #define DECLINE          "DECLINE"
+#define DISCONNECT       "DISCONNECT"
 #define DEVICE_NAME      "DEVICE_NAME"
 #define FILE_BYTE        "FILE_BYTE"
 #define FILE_BYTE_TO     "FILE_BYTE_TO"
-#define DISCONNECT       "DISCONNECT"
-#define FILE_CHUNK_SIZE  1024
+#define FILE_SEND        "FILE_SEND"
+#define FILE_SEND_END    "FILE_SEND_END"
 
 using namespace std;
 
@@ -33,6 +33,7 @@ class Server {
 		void SendClientFile(string& inputFolder, char* buffer, const int& bufferSize);
 		void SaveFileData(const int& bufferSize, const char* fileName);
 		bool FolderExists(const wchar_t* folderPath) const;
+		bool FileExists(const wchar_t* filePath) const;
 		vector<string> MessageParse(string message, int& msgLen = _LENGTH, const char seperator = '|');
 		string GetClientDeviceName(char* buffer, int& buffSize);
 		string GetIPv4() const ;
