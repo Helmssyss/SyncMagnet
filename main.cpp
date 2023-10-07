90bf7fa3e402c9cbf9da4edcfd09a3b2cb8150d2
Helmsys <84701901+Helmssyss@users.noreply.github.com>
2023-10-07 11:27:13 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,6 @@
 #include "Public/server.h"
-#include <iostream>
 
 int main(int argc, char* *argv) {
-	//printf("You have entered %d arguments:\n", argc);
-
-	//for (int i = 0; i < argc; i++) {
-	//	printf("%s\n", argv[i]);
-	//}
 	Server* server = new Server;
 
 	server->Setup();
