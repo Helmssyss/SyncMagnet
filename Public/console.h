5ba846b1c1ee0137842f214d8b316b2f92b6af78
Helmsys <84701901+Helmssyss@users.noreply.github.com>
2023-10-08 00:33:32 +0300

    Update console.h

--- Public/console.h
+++ Public/console.h
@@ -8,6 +8,7 @@ using namespace std;
 
 namespace console {
     constexpr const char* PURPLE = "\033[95m";
+    constexpr const char* MAGENTA = "\033[35m";
     constexpr const char* CYAN = "\033[96m";
     constexpr const char* DARKCYAN = "\033[36m";
     constexpr const char* BLUE = "\033[94m";
@@ -16,24 +17,27 @@ namespace console {
     constexpr const char* RED = "\033[91m";
     constexpr const char* BOLD = "\033[1m";
     constexpr const char* DEFAULT = "\033[0m";
-    constexpr const char* BANNER = "-*-SyncMagnet-*-\n";
-
+    constexpr const char* BANNER = R"(
+    .---.             ,-,-,-.                   .
+    \___  . . ,-. ,-. `,| | |   ,-. ,-. ,-. ,-. |- 
+        \ | | | | |     | ; | . ,-| | | | | |-' |  
+    `---' `-| ' ' `-'   '   `-' `-^ `-| ' ' `-' `' 
+           /|        v0.0.6          ,|            
+          `-'       Helmssyss        `')";
 
     inline void SaveFileQuestionDisplay() { printf("%s[~] %sSave File(s) [%sy-Y%s/%sn-N%s]%s\n", RED, PURPLE, YELLOW, PURPLE, YELLOW, PURPLE, DEFAULT); }
     inline void AlertMessage(const char* msg) { printf("\n%s[~] %s%s%s\n", RED, PURPLE, msg, DEFAULT); }
     inline void ErrorDisplay(const char* msg) { printf("\n%s[!] %s%s%s\n", PURPLE, RED, msg, DEFAULT); }
 
     void Banner();
-    void Header();
+    void Header(string ip);
     void QuitMessage();
-    void Body(string chr);
+    void Body();
     void ConnectedClientDisplay(const char* client);
     void GetFileAlertMessage(const char* fileName, int fileSize, bool getIsMultipleFile = false);
     void UploadFileDisplay(const int& uploadByte, const long& fileSize);
     void CompleteUploadFileDisplay(const long& fileSize);
     void DownloadFileDisplay(const int& downloadByte, const long& fileSize);
     
-    string Input();
-
-    
+    string Input();    
 }
