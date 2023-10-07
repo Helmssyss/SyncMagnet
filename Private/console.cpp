0ccfc02e6fb584bc66a24f145e97912c47643c09
Helmsys <84701901+Helmssyss@users.noreply.github.com>
2023-10-08 00:33:05 +0300

    Update console.cpp

--- Private/console.cpp
+++ Private/console.cpp
@@ -1,30 +1,31 @@
-﻿#include <iomanip>
+#include <iomanip>
 
 #include "../Public/console.h"
 
 void console::Banner() {
-    printf("%s%s\n%s", PURPLE, BANNER, DEFAULT);
+    std::cout << PURPLE << BANNER << DEFAULT <<std::endl;
 }
 
-void console::Header() {
+void console::Header(string ip) {
     Banner();
-    printf("%s[0] %sGet File \t\t%s[1] %sSent File%s\n", RED, CYAN, RED, CYAN, DEFAULT);
-    printf("%s[x] %sClose%s\n", RED, CYAN, DEFAULT);
+    printf("\t%sYour Local IPv4 Address : %s%s\n\n", MAGENTA, ip.c_str(), DEFAULT);
+    printf("%s[0] %sGet File(s)%s\n", PURPLE, MAGENTA, DEFAULT);
+    printf("%s[1] %sSent File%s\n", PURPLE,MAGENTA, DEFAULT);
+    printf("%s[x] %sClose%s\n", PURPLE, MAGENTA, DEFAULT);
 }
 
-void console::Body(string chr) {
-    printf("\n%s[~] %sConnection Started\n", RED, PURPLE);
-    printf("%s[?] %sConnect From Your Address >>%s%s%s<<%s\n", RED, PURPLE, YELLOW, chr.c_str(), PURPLE, DEFAULT);
-    printf("%s[~] %sWaiting For Connection%s\n", RED, PURPLE, DEFAULT);
+void console::Body() {
+    printf("\n%s[~] %sConnection Started\n", PURPLE, PURPLE);
+    printf("%s[~] %sWaiting For Connection%s\n", PURPLE, PURPLE, DEFAULT);
 }
 
 void console::ConnectedClientDisplay(const char* client) {
-    printf("\r%s[*] %sConnected Device >>%s%s%s<<%s\n", RED, PURPLE, YELLOW, client, PURPLE, DEFAULT);
+    printf("\r%s[*] %sConnected Device >>%s%s%s<<%s\n", PURPLE, PURPLE, MAGENTA, client, PURPLE, DEFAULT);
 }
 
 std::string console::Input() {
     std::string input;
-    printf("%s[*] %shelmsys@sync_magnet:~$ %s", RED, PURPLE, YELLOW);
+    printf("%s[*] %shelmsys@sync_magnet:~$ %s", PURPLE, PURPLE, YELLOW);
     std::getline(std::cin, input);
     return input;
 }
@@ -34,13 +35,13 @@ void console::GetFileAlertMessage(const char* fileName, int fileSize, bool getIs
     maxFileNameLength = std::max(maxFileNameLength, static_cast<int>(strlen(fileName)));
     if(getIsMultipleFile){
         std::string spacing(maxFileNameLength - strlen(fileName), ' ');
-        std::cout << RED << "[#] "
+        std::cout << PURPLE << "[#] "
             << YELLOW << fileName << spacing << "  " << std::fixed << std::setprecision(6) << " " << (float)fileSize / (1024 * 1024) << " "
             << PURPLE << ((float)fileSize / (1024 * 1024) >= 1 ? "MB" : "KB")<< DEFAULT << std::endl;
         
     }else {
         printf("%s[!] %sFile Name: %s%s\n%s[!] %sFile Size: %s%f %s%s\n",
-            RED, PURPLE, YELLOW, fileName, RED, PURPLE, YELLOW, (float)fileSize / (1024 * 1024),
+            PURPLE, PURPLE, YELLOW, fileName, PURPLE, PURPLE, YELLOW, (float)fileSize / (1024 * 1024),
             (float)fileSize / (1024 * 1024) >= 1 ? "MB" : "KB", DEFAULT
         );
     }
@@ -48,7 +49,7 @@ void console::GetFileAlertMessage(const char* fileName, int fileSize, bool getIs
 
 void console::UploadFileDisplay(const int& uploadByte, const long& fileSize) {
     printf("\r%s[~] %s%f/%f %s%s Uploading%s",
-        RED, YELLOW, (float)uploadByte / (1024 * 1024),
+        PURPLE, YELLOW, (float)uploadByte / (1024 * 1024),
         (float)fileSize / (1024 * 1024), PURPLE,
         (uploadByte / (1024 * 1024)) >= 1 ? "MB" : "KB", DEFAULT
     );
@@ -56,13 +57,13 @@ void console::UploadFileDisplay(const int& uploadByte, const long& fileSize) {
 
 void console::CompleteUploadFileDisplay(const long& fileSize) {
     printf("\r%s[*] %s%f %s%s File Sent Upload%s\n\a",
-        RED, YELLOW, (float)fileSize / (1024 * 1024), PURPLE,
+        PURPLE, YELLOW, (float)fileSize / (1024 * 1024), PURPLE,
         (float)(fileSize / (1024 * 1024)) >= 1 ? "MB" : "KB", DEFAULT
     );
 }
 
 void console::DownloadFileDisplay(const int& downloadByte, const long& fileSize) {
-    printf("\r%s[~] %s%f/%f %s%s Downloading File%s", RED, YELLOW,
+    printf("\r%s[~] %s%f/%f %s%s Downloading File%s", PURPLE, YELLOW,
         (float)downloadByte / (1024 * 1024), (float)fileSize / (1024 * 1024),
         PURPLE, (fileSize / (1024 * 1024)) >= 1 ? "MB" : "KB", DEFAULT);
 }
@@ -70,7 +71,7 @@ void console::DownloadFileDisplay(const int& downloadByte, const long& fileSize)
 void console::QuitMessage() {
     system("cls");
     Banner();
-    printf("%s[!] %sDisconnected%s\n", RED, PURPLE, DEFAULT);
-    printf("%s[!] %sReopen to connect%s\n", RED, PURPLE, DEFAULT);
-    printf("%s[!] %sPress 'Enter'%s\n", RED, PURPLE, DEFAULT);
+    printf("%s[!] %sDisconnected%s\n", PURPLE, PURPLE, DEFAULT);
+    printf("%s[!] %sReopen to connect%s\n", PURPLE, PURPLE, DEFAULT);
+    printf("%s[!] %sPress 'Enter'%s\n", PURPLE, PURPLE, DEFAULT);
 }
